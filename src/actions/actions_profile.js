a2f77d76fa5636fc02c49289efd710191c1d7d0d
Anush <afr741@mun.ca>
2019-08-20 22:34:04 -0230

    refactoring the access token
    -added a variable to store the access token

--- src/actions/actions_profile.js
+++ src/actions/actions_profile.js
@@ -1,11 +1,13 @@
 export const PROFILE_FETCHED = 'PROFILE_FETCHED';
 export const PROFILE_EDITED = 'PROFILE_EDITED';
 
+//personal access token that needs to be GENERATED FOR EACH USER.
+  const personalAccessToken = "token 37fbddda5e5c16e5183c757423b801f6cae2e55b";
 export function fetchProfile() {
   return (dispatch) => {
 
-    let header = new Headers({"Content-Type":"application/json", "Authorization":"token a7b95fd6a68a9f6d81ec365cf693ba6e0ae691ec"});
-    return fetch('https://api.github.com/users/shaunvoner', {
+    let header = new Headers({"Content-Type":"application/json", "Authorization": {personalAccessToken}});
+    return fetch('https://api.github.com/users/afr741', {
       method: 'GET',
       headers: header
     })
@@ -20,7 +22,7 @@ export function fetchProfile() {
 export function saveProfile(profile) {
   return (dispatch) => {
 
-    let header = new Headers({"Content-Type":"application/json", "Authorization":"token a7b95fd6a68a9f6d81ec365cf693ba6e0ae691ec"});
+    let header = new Headers({"Content-Type":"application/json", "Authorization": {personalAccessToken} });
     return fetch('https://api.github.com/user', {
       method: 'PATCH',
       headers: header,
